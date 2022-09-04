970bf8139eb82e3b42f48e63e586928f8726979e
successfuldesire <100515398+successfuldesire@users.noreply.github.com>
2022-09-04 08:36:01 +0300

    Add files via upload
    Возник интересный момент, если делать сокращение до 2 чисел после запятой, то во втором примере считает правильно, а в первом почему-то округляет, хотя если без обрезки по знакам после запятой делать, то полное число верное)

---
+++ ex4.py
@@ -0,0 +1,11 @@
+chetvert = int(input("Введите номер четверти: "))
+if chetvert == 1:
+    print("Значения координат: x > 0 и y > 0")
+if chetvert == 2:
+    print("Значения координат: x < 0 и y > 0")
+if chetvert == 3:
+    print("Значения координат: x < 0 и y < 0")
+if chetvert == 4:
+    print("Значения координат: x > 0 и y < 0")
+if chetvert == 0:
+    print("Вы ввели несуществующую четверть")
